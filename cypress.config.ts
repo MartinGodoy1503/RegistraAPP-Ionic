f18ff9640c8b5b8a3c3bd0a631bf67e166db4121
MartinAndres1503 <martin.andres1503@gmail.com>
2024-12-06 01:38:02 -0300

    Finalizacion Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
     baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
       // Aquí puedes configurar eventos si es necesario
--- cypress/e2e/loginspec.cy.ts
+++ cypress/e2e/loginspec.cy.ts
@@ -1,31 +1,35 @@
 describe('Login Page', () => {
-  
+
   beforeEach(() => {
     // Visitar la página de login
     cy.visit('/login');
   });
 
-  it('should display the login form correctly', () => {
-    cy.get('ion-input[formControlName="username"]').should('be.visible');
-    cy.get('ion-input[formControlName="password"]').should('be.visible');
-    cy.get('ion-button[type="submit"]').should('be.visible');
+  it('Deberia mostrar el formulario de login correctamente', () => {
+    cy.get('ion-input[formControlName="username"]').should('exist').and('be.visible').type('incorrectUser');
+    cy.get('ion-input[formControlName="password"]').should('exist').and('be.visible').type('incorrectPass');
+    cy.get('ion-button[type="submit"]').click();
   });
 
-  it('should show an error message if credentials are invalid', () => {
-    cy.get('input[name="username"]').type('incorrectUser');
-    cy.get('input[name="password"]').type('incorrectPass');
+  it('Deberia mostrar de credenciales inválidas si estas lo son', () => {
+    // Escribe credenciales inválidas
+    cy.get('ion-input[formControlName="username"]').type('incorrectUser');
+    cy.get('ion-input[formControlName="password"]').type('incorrectPass');
     cy.get('ion-button[type="submit"]').click();
-    
-    // Asumiendo que el servicio de autenticación muestra un mensaje de error
-    cy.contains('Credenciales incorrectas. Intenta de nuevo.').should('be.visible');
+
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.include('Error durante la autenticación');
+    });
+
   });
+  
 
-  it('should navigate to home on successful login', () => {
-    cy.get('input[name="username"]').type('validUser');
-    cy.get('input[name="password"]').type('validPass');
+  it('Debería redirijar a home despues de ingresar credenciales válidas', () => {
+    cy.get('ion-input[formControlName="username"]').type('mgarcia');
+    cy.get('ion-input[formControlName="password"]').type('Admin123!');
     cy.get('ion-button[type="submit"]').click();
 
-    // Verifica que después de iniciar sesión, se navegue a la página de inicio
+    // Verifica que después de iniciar sesión, se redirija a la pag de inicio
     cy.url().should('include', '/home');
   });
 });
