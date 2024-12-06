4d1da23e438a1932a7f08e6b81b71891394cc11a
MartinAndres1503 <martin.andres1503@gmail.com>
2024-12-05 22:08:42 -0300

    Ajustes de Cypress

--- cypress/e2e/loginspec.cy.ts
+++ cypress/e2e/loginspec.cy.ts
@@ -1,57 +1,31 @@
-// cypress/e2e/login.spec.cy.ts
-
 describe('Login Page', () => {
+  
   beforeEach(() => {
-    cy.visit('/login'); 
+    // Visitar la página de login
+    cy.visit('/login');
   });
 
   it('should display the login form correctly', () => {
-    cy.get('ion-card-title').should('contain', 'Iniciar Sesión');
     cy.get('ion-input[formControlName="username"]').should('be.visible');
     cy.get('ion-input[formControlName="password"]').should('be.visible');
-    cy.get('ion-button[type="submit"]').should('contain', 'Ingresar');
+    cy.get('ion-button[type="submit"]').should('be.visible');
   });
 
-  it('should show validation errors for empty fields', () => {
-    cy.get('ion-button[type="submit"]').click();
-
-    cy.get('div').contains('El nombre de usuario es obligatorio.').should('be.visible');
-    cy.get('div').contains('Ingrese su contraseña.').should('be.visible');
-  });
-
-  it('should log in with valid credentials', () => {
-    cy.get('ion-input[formControlName="username"]').type('JohnDoe01');
-    cy.get('ion-input[formControlName="password"]').type('password123');
-
-    cy.intercept('POST', '/login', {
-      statusCode: 200,
-      body: {
-        id: '1',
-        username: 'JohnDoe01',
-      },
-    }).as('loginRequest');
-
-    cy.get('ion-button[type="submit"]').click();
-
-    cy.wait('@loginRequest');
-    cy.url().should('include', '/home');
-    cy.get('p').should('contain', 'Bienvenido/a!');
-  });
-
-  it('should show an error message for incorrect credentials', () => {
-    cy.get('ion-input[formControlName="username"]').type('wrongUser');
-    cy.get('ion-input[formControlName="password"]').type('wrongPassword');
-
-    cy.intercept('POST', '/login', {
-      statusCode: 401,
-      body: {
-        message: 'Credenciales incorrectas',
-      },
-    }).as('loginRequestFailed');
-
+  it('should show an error message if credentials are invalid', () => {
+    cy.get('input[name="username"]').type('incorrectUser');
+    cy.get('input[name="password"]').type('incorrectPass');
     cy.get('ion-button[type="submit"]').click();
     
-    cy.wait('@loginRequestFailed');
-    cy.get('div').contains('Credenciales incorrectas. Intenta de nuevo.').should('be.visible');
+    // Asumiendo que el servicio de autenticación muestra un mensaje de error
+    cy.contains('Credenciales incorrectas. Intenta de nuevo.').should('be.visible');
+  });
+
+  it('should navigate to home on successful login', () => {
+    cy.get('input[name="username"]').type('validUser');
+    cy.get('input[name="password"]').type('validPass');
+    cy.get('ion-button[type="submit"]').click();
+
+    // Verifica que después de iniciar sesión, se navegue a la página de inicio
+    cy.url().should('include', '/home');
   });
 });
